34cf721f1d0a1c522f0b7cd35cb364ff868fe2c1
danhavey <80353094+danhavey@users.noreply.github.com>
2025-03-20 13:37:57 -0700

    Create slider-code.js
    Created 20Mar2025

---
+++ html/slider-code.js
@@ -0,0 +1,63 @@
+/*  On Window Load - Fix the Arrows and Dots  */
+  
+  $(window).on('load', function() {
+    $('button.slick-prev, button.slick-next, .slick-dots button').addClass("fas");
+    $('button.slick-prev, button.slick-next, .slick-dots button').text('');
+    
+/*  Set Slider Sections All To The Same Height  */
+    
+    $('[data-title*="sliderColumn"] .slick-track').each(function() {
+      let trackHeight = $(this).css('height');
+      console.log(trackHeight);
+      $(this).find('.slick-slide').css('height', trackHeight);
+      $(this).find('.slick-slide > div').css('height', trackHeight);
+      $(this).find('.slick-slide > div > .container').css('height', trackHeight);
+    });
+  });
+  
+/*  On Responsive Breakpoint - Fix the Arrows and Dots  */
+  
+$('[data-title*="mySlider"]').on('breakpoint', function(event, slick, breakpoint){
+  $('button.slick-prev, button.slick-next, .slick-dots button').addClass("fas");
+  $('button.slick-prev, button.slick-next, .slick-dots button').text('');
+});
+  
+/*****     *****     *****/    
+  
+//  Run on initialization - if cloned slide is clicked it gets temp-current
+  
+  $('[data-title*="mySlider"] .slick-slide.slick-cloned').on('click', function() {
+    let clickedSlide = $(this).attr('data-slick-index');
+    $('[data-title*="mySlider"] .slick-slide.slick-cloned').removeClass('temp-current');
+    $('[data-slick-index="' + clickedSlide + '"]').addClass('temp-current');
+  });
+  
+// Add the temp-current class to the cloned nextSlide
+  
+  $('[data-title*="mySlider"]').on('beforeChange', function(event, slick, currentSlide, nextSlide) {
+    let firstClone = slick.slideCount; 
+    let lastSlide = firstClone - 1;
+    let autoPlay = slick.options.autoplay;
+    
+    if ( autoPlay == true ) {
+      if ( currentSlide == lastSlide ) {
+        if ( nextSlide == 0 ) {
+          $('[data-slick-index="' + firstClone + '"]').addClass('temp-current');
+        }
+      }
+      if ( currentSlide == 0 ) {
+        $('[data-slick-index="' + firstClone + '"]').removeClass('temp-current');
+      }
+    } 
+  }); 
+
+//  Remove temp-current after change
+  
+  $('[data-title*="mySlider"]').on('afterChange', function(event, slick, currentSlide) {
+    let firstClone = slick.slideCount; 
+    let lastSlide = firstClone - 1;
+    if ( currentSlide >= 0 && currentSlide <= lastSlide ) {
+      $('[data-title*="mySlider"] .slick-slide.slick-cloned').removeClass('temp-current');
+    };
+  });
+  
